a4a2297e9792dadd858494f0c09f19171dc5b022
NataliiaOsadcha <nat.osadchaya@gmail.com>
2023-08-16 13:40:36 +0300

    add arr products

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,20 +1,19 @@
 import "./Article.css";
-import picture from "../../assets/lenovo-laptop-y50-cover-6_01 2.png";
 import { BiCart } from "react-icons/bi";
 
-const Article = () => {
+const Article = ({ product }) => {
   return (
     <div className="article-box">
-      <div className="article-img" src={picture} alt="picture">
-        <img src={picture} alt="picture" />
+      <div className="article-img">
+        <img src={product.img} alt="picture" />
       </div>
-      <h3>Ноутбук Lenovo Y50-70 Aluminum Black</h3>
+      <h3>{product.name}</h3>
       <div className="article-body">
-        <div className="article-price">25000₴</div>
-        <div className="article-quantity">Кількість: 5</div>
+        <div className="article-price">{product.price}</div>
+        <div className="article-quantity">{product.quantity}</div>
       </div>
       <div className="article-footer">
-        <div className="article-footer-text">Готовий до відправки</div>
+        <div className="article-footer-text">{product.basket}</div>
         <div className="bye-icon">
           <BiCart size={28} />
         </div>
--- src/containers/ProductPreview/ProductPreview.js
+++ src/containers/ProductPreview/ProductPreview.js
@@ -2,6 +2,36 @@ import "./ProductPreview.css";
 import logo from "../../assets/logo_main_white.svg";
 import Article from "../../components/Article/Article";
 
+const arr = [
+  {
+    id: 1,
+    img: "../../assets/lenovo-laptop-y50-cover-6_01 2.png",
+    name: "Ноутбук Lenovo Y50-70 Aluminum Black",
+    price: "25000₴",
+    quantity: "Кількість: 5шт",
+    logo: "",
+    basket: "Готовий до відправки",
+  },
+  {
+    id: 2,
+    img: "../../assets/lenovo-laptop-y50-cover-6_01 2.png",
+    name: "Ноутбук Lenovo Y50-70 Aluminum Black",
+    price: "25000₴",
+    quantity: "Кількість: 5шт",
+    logo: "",
+    basket: "Готовий до відправки",
+  },
+  {
+    id: 3,
+    img: "../../assets/lenovo-laptop-y50-cover-6_01 2.png",
+    name: "Ноутбук Lenovo Y50-70 Aluminum Black",
+    price: "25000₴",
+    quantity: "Кількість: 5шт",
+    logo: "",
+    basket: "Готовий до відправки",
+  },
+];
+
 function ProductPreview() {
   return (
     <div className="container">
@@ -9,12 +39,9 @@ function ProductPreview() {
         <img className="img-logo-white" src={logo} alt="logo" />
       </div>
       <div className="products-flex-box">
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
+        {arr.map((product) => (
+          <Article key={product.id} product={product} />
+        ))}
       </div>
     </div>
   );
